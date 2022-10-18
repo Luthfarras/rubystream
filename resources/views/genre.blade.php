56f2bb8bbb9cd2e3a42d253f429fc8c84e2d2fe8
handimanny <handimanny10@gmail.com>
2022-10-18 17:16:46 +0700

    modal-dialog-centered

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -1,5 +1,109 @@
 @extends('template')
 
 @section('content')
-<h1 class="text-white">Ini halaman genre</h1>
+
+<div class="form-group container">
+    <a class="btn btn-dark " href="#" data-toggle="modal" data-target="#inputModal">
+        Input Genre
+    </a>
+</div>
+
+<form>
+    <div class="modal fade" id="inputModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content text-white footer">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to input</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group container">
+                        <input type="text" class="form-control" id="genre" placeholder="Input Genre">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-dark text-white">Input</button>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<table class="table container text-white">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name Genre</th>
+      <th scope="col">Update</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Belum</td>
+      <td>
+    <a class="btn btn-success" href="#" data-toggle="modal" data-target="#editModal">
+        Edit
+    </a>
+        |
+    <a class="btn btn-danger" href="#" data-toggle="modal" data-target="#deleteModal">
+        Delete
+    </a>
+    </td>
+    </tr>
+  </tbody>
+</table>
+
+<form>    
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content text-white footer">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to edit</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group container">
+                        <input type="text" class="form-control" id="genre" placeholder="Edit Genre">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success text-white">Update</button>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<form>    
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+        <!-- modal-dialog-centered -->
+            <div class="modal-content text-white footer">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to delete</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group container">
+                        Are you sure delete it?
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-danger text-white">Delete</button>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
 @endsection
