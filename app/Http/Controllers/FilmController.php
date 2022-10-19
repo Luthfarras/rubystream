d6157b105c0c12f7219b432284528b97ea478903
Luthfarras <luthfarras_tresanizega_27rpl@student.smktelkom-mlg.sch.id>
2022-10-19 08:35:49 +0700

    add dashboard

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Film;
 
 class FilmController extends Controller
 {
@@ -13,7 +14,8 @@ class FilmController extends Controller
      */
     public function index()
     {
-        //
+        $data = Film::all();
+        return view('dashboard', compact('data'));
     }
 
     /**
