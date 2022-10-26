99eede29ab981ddc3a48dc65974993280eddd749
asaddin02 <114129501+asaddin02@users.noreply.github.com>
2022-10-26 15:05:23 +0700

    arif

--- resources/views/film/create.blade.php
+++ resources/views/film/create.blade.php
@@ -2,53 +2,69 @@
     <div class="modal-dialog modal-md" role="document">
         <div class="modal-content text-white footer p-0">
 
-        <form action="{{ url('genre')}}" method="POST" enctype="multipart/form-data">
+            <form action="{{ url('genre') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-            <div class="modal-body">
-                <label class="mb-3" style="font-size: 1.4rem;">Input Film</label>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('nama_film') is-invalid @enderror" id="nama_film" placeholder="Film Name" name="nama_film">
+                <div class="modal-body">
+                    <label class="mb-3" style="font-size: 1.4rem;">Input Film</label>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                    <div class="form-group mt-2">
+                        <input type="text"
+                            class="form-control form-control-sm @error('nama_film') is-invalid @enderror" id="nama_film"
+                            placeholder="Film Name" name="nama_film">
+                    </div>
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control form-control-sm @error('studio') is-invalid @enderror"
+                            id="studio" placeholder="Studio" name="studio">
+                    </div>
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control form-control-sm @error('harga') is-invalid @enderror"
+                            id="harga" placeholder="Price" name="harga">
+                    </div>
+                    <div class="form-group mt-2">
+                        <input type="text"
+                            class="form-control form-control-sm @error('tahun_rilis') is-invalid @enderror"
+                            id="tahun_rilis" placeholder="Release Year" name="tahun_rilis">
+                    </div>
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control form-control-sm @error('aktor') is-invalid @enderror"
+                            id="aktor" placeholder="Actors" name="aktor">
+                    </div>
+                    <div class="form-group mt-2">
+                        <input type="text"
+                            class="form-control form-control-sm @error('sinopsis') is-invalid @enderror" id="sinopsis"
+                            placeholder="Sinopsis" name="sinopsis">
+                    </div>
+                    <div class="form-group mt-2">
+                        <select class=" @error('sinopsis') is-invalid @enderror">
+                            <option>1</option>
+                        </select>
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="cover" class="form-label">Cover</label>
+                        <input type="file" class=" @error('tahun_rilis') is-invalid @enderror" id="tahun_rilis"
+                            placeholder="Release Year" name="tahun_rilis">
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="cover" class="form-label">Cover</label>
+                        <input type="file" class=" @error('tahun_rilis') is-invalid @enderror" id="tahun_rilis"
+                            placeholder="Release Year" name="tahun_rilis">
+                    </div>
+                    {{-- <div class="form-group mt-2">
+                    </div> --}}
+                    <div class="form-group mt-2">
+                        <label for="cover" class="form-label">Cover</label>
+                        <input type="file" class="@error('cover') is-invalid @enderror" id="cover"
+                            placeholder="Cover" name="cover" value="{{ old('cover') }}">
+                    </div>
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-sm btn-dark text-white">Input</button>
+                        <button class="btn btn-sm btn-secondary" type="button" data-dismiss="modal"
+                            style="float: right;">Cancel</button>
+                    </div>
                 </div>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('studio') is-invalid @enderror" id="studio" placeholder="Studio" name="studio">
-                </div>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('harga') is-invalid @enderror" id="harga" placeholder="Price" name="harga">
-                </div>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('tahun_rilis') is-invalid @enderror" id="tahun_rilis" placeholder="Release Year" name="tahun_rilis">
-                </div>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('aktor') is-invalid @enderror" id="aktor" placeholder="Actors" name="aktor">
-                </div>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('sinopsis') is-invalid @enderror" id="sinopsis" placeholder="Sinopsis" name="sinopsis">
-                </div>
-                <select name="genre_id" id="" class="form-control form-control-sm @error('tahun_rilis') is-invalid @enderror">
-                    {{-- @foreach ($data as $item) --}}
-                    <option value="">isadnoifhda</option>
-                    {{-- @endforeach --}}
-                </select>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('tahun_rilis') is-invalid @enderror" id="tahun_rilis" placeholder="Release Year" name="tahun_rilis">
-                </div>
-                <div class="form-group mt-2">
-                    <input type="text" class="form-control form-control-sm @error('tahun_rilis') is-invalid @enderror" id="tahun_rilis" placeholder="Release Year" name="tahun_rilis">
-                </div>
-                <div class="form-group mt-2">
-                </div>
-                <div class="form-group mt-2">
-                    <input type="file" class="@error('cover') is-invalid @enderror" id="cover" placeholder="Cover" name="cover" value="{{old('cover')}}">
-                </div>
-                <div class="mt-3">
-                    <button type="submit" class="btn btn-sm btn-dark text-white">Input</button>
-                    <button class="btn btn-sm btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                </div>
-            </div>
-        </form>
+            </form>
 
         </div>
     </div>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -12,7 +12,7 @@
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
+        rel="stylesheet">
 
     <!-- Css Styles -->
     <link rel="stylesheet" href="{{ asset('anime-main/css/bootstrap.min.css') }}" type="text/css">
@@ -26,10 +26,24 @@
     <link rel="stylesheet" href="{{ asset('anime-main/css/style.css') }}" type="text/css">
 
 
-<!-- start pagination stylesheet -->
-<link rel="stylesheet" href="{{ asset('https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css') }}">
-<!-- end pagination stylesheet -->
+    <!-- start pagination stylesheet -->
+    <link rel="stylesheet" href="{{ asset('https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css') }}">
+    <!-- end pagination stylesheet -->
+    <style>
+        .nice-select {
+            float: none;
+        }
 
+        .nice-select ul li {
+            color: black;
+            width: 29.3rem;
+            padding: none;
+        }
+
+        .nice-select span {
+            color: black;
+        }
+    </style>
 </head>
 
 <body>
@@ -54,33 +68,37 @@
                         <nav class="header__menu mobile-menu">
                             <ul>
                                 @guest
-                                @if(Route::has('login'))
-                                <li class="@if(Request::is('/*')) active @endif"><a href="/">Homepage</a></li>
-                                <li><a>Categories <span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="">Categories</a></li>
-                                        <li><a href="">Film Details</a></li>
-                                        <li><a href="">Film Watching</a></li>
-                                        <li><a href="">Blog Details</a></li>
-                                        <!-- <li><a href="">Sign Up</a></li>
-                                        <li><a href="">Login</a></li> -->
-                                    </ul>
-                                </li>
-                                @endif
+                                    @if (Route::has('login'))
+                                        <li class="@if (Request::is('/*')) active @endif"><a
+                                                href="/">Homepage</a></li>
+                                        <li><a>Categories <span class="arrow_carrot-down"></span></a>
+                                            <ul class="dropdown">
+                                                <li><a href="">Categories</a></li>
+                                                <li><a href="">Film Details</a></li>
+                                                <li><a href="">Film Watching</a></li>
+                                                <li><a href="">Blog Details</a></li>
+                                                <!-- <li><a href="">Sign Up</a></li>
+                                                                                                                <li><a href="">Login</a></li> -->
+                                            </ul>
+                                        </li>
+                                    @endif
                                 @else
-                                <li class="@if(Request::is('/*')) active @endif"><a href="/">Homepage</a></li>
-                                <li><a>Categories <span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="">Categories</a></li>
-                                        <li><a href="">Film Details</a></li>
-                                        <li><a href="">Film Watching</a></li>
-                                        <li><a href="">Blog Details</a></li>
-                                        <!-- <li><a href="">Sign Up</a></li>
-                                        <li><a href="">Login</a></li> -->
-                                    </ul>
-                                </li>
-                                <li class="@if(Request::is('film*')) active @endif"><a href="/film">List Film</a></li>
-                                <li class="@if(Request::is('genre*')) active @endif"><a href="/genre">Genre</a></li>
+                                    <li class="@if (Request::is('/*')) active @endif"><a
+                                            href="/">Homepage</a></li>
+                                    <li><a>Categories <span class="arrow_carrot-down"></span></a>
+                                        <ul class="dropdown">
+                                            <li><a href="">Categories</a></li>
+                                            <li><a href="">Film Details</a></li>
+                                            <li><a href="">Film Watching</a></li>
+                                            <li><a href="">Blog Details</a></li>
+                                            <!-- <li><a href="">Sign Up</a></li>
+                                                                                                                <li><a href="">Login</a></li> -->
+                                        </ul>
+                                    </li>
+                                    <li class="@if (Request::is('film*')) active @endif"><a href="/film">List
+                                            Film</a></li>
+                                    <li class="@if (Request::is('genre*')) active @endif"><a href="/genre">Genre</a>
+                                    </li>
                                 @endguest
                             </ul>
                         </nav>
@@ -91,15 +109,16 @@
                     <div class="header__nav">
                         <nav class="header__menu mobile-menu">
                             <ul>
-                              <li>
-                                <a href="#" class="search-switch text-light"><i class="icon_cart"></i><span class="position-absolute badge rounded-pill bg-danger" style="font-size:12px;">5</span></a>
-                              </li>
-
+                                <li>
+                                    <a href="#" class="search-switch text-light"><i class="icon_cart"></i><span
+                                            class="position-absolute badge rounded-pill bg-danger"
+                                            style="font-size:12px;">5</span></a>
+                                </li>
                                 <a href="#" class="search-switch text-light"><span class="icon_search"></span></a>
                                 <li>
                                     <a>
-                                        @if(Auth::user())
-                                        {{ Auth::user()->name }}
+                                        @if (Auth::user())
+                                            {{ Auth::user()->name }}
                                         @endif
                                         <span class="icon_profile"></span>
                                     </a>
@@ -107,20 +126,23 @@
 
                                         <li>
                                             @guest
-                                            @if (Route::has('login'))
-                                            <a class="dropdown-item text-dark" href="{{ route('login') }}">Login</a>
-                                            <a class="dropdown-item text-dark" href="{{ route('register') }}">Register</a>
-                                            @endif
+                                                @if (Route::has('login'))
+                                                    <a class="dropdown-item text-dark"
+                                                        href="{{ route('login') }}">Login</a>
+                                                    <a class="dropdown-item text-dark"
+                                                        href="{{ route('register') }}">Register</a>
+                                                @endif
                                             @else
-                                            <a class="dropdown-item text-dark" href="{{ route('home') }}">Profile</a>
-                                            <a class="dropdown-item text-dark" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault();
+                                                <a class="dropdown-item text-dark" href="{{ route('home') }}">Profile</a>
+                                                <a class="dropdown-item text-dark" href="{{ route('logout') }}"
+                                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                                        {{ __('Logout') }}
-                                            </a>
+                                                    {{ __('Logout') }}
+                                                </a>
                                             @endguest
 
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                class="d-none">
                                                 @csrf
                                             </form>
 
@@ -131,85 +153,94 @@
                         </nav>
                     </div>
 
-                        <!-- <a href="./login.html"><span class="icon_profile"></span></a> -->
-                    </div>
+                    <!-- <a href="./login.html"><span class="icon_profile"></span></a> -->
                 </div>
             </div>
-            <div id="mobile-menu-wrap"></div>
+        </div>
+        <div id="mobile-menu-wrap"></div>
         </div>
     </header>
     <!-- Header End -->
     <section class="all">
-      @yield('content')
+        @yield('content')
     </section>
 
 
-<!-- Footer Section Begin -->
-<footer class="footer">
-    <div class="page-up">
-        <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="footer__logo">
-                    <a href="./index.html"><img src="{{ asset('anime-main/img/logo.png') }}" alt=""></a>
+    <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="page-up">
+            <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="footer__logo">
+                        <a href="./index.html"><img src="{{ asset('anime-main/img/logo.png') }}" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="footer__nav">
+                        <ul>
+                            <li><a href="/">Homepage</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/film">List Film</a></li>
+                            <li><a href="/genre">Genre</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | This template is made with <i
+                            class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com"
+                            target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+
                 </div>
             </div>
-            <div class="col-lg-6">
-                <div class="footer__nav">
-                    <ul>
-                        <li><a href="/">Homepage</a></li>
-                        <li><a href="/profile">Profile</a></li>
-                        <li><a href="/film">List Film</a></li>
-                        <li><a href="/genre">Genre</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
 
-              </div>
-          </div>
-      </div>
-  </footer>
-  <!-- Footer Section End -->
-
-  <!-- Search model Begin -->
-  <div class="search-model">
-    <div class="h-100 d-flex align-items-center justify-content-center">
-        <div class="search-close-switch"><i class="icon_close"></i></div>
-        <form class="search-model-form" action="{{ route('search') }}" method="get">
-            <input type="text" id="search-input" name="search" placeholder="Search here....." value="{{ old('search') }}">
-            <button type="submit" class="icon_search text-white btn btn-outline"></button>
-        </form>
+    <!-- Search model Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch"><i class="icon_close"></i></div>
+            <form class="search-model-form" action="{{ route('search') }}" method="get">
+                <input type="text" id="search-input" name="search" placeholder="Search here....."
+                    value="{{ old('search') }}">
+                <button type="submit" class="icon_search text-white btn btn-outline"></button>
+            </form>
+        </div>
     </div>
-</div>
-<!-- Search model end -->
+    <!-- Search model end -->
 
-<!-- Js Plugins -->
-<script src="{{ asset('anime-main/js/jquery-3.3.1.min.js') }}"></script>
-<script src="{{ asset('anime-main/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('anime-main/js/player.js') }}"></script>
-<script src="{{ asset('anime-main/js/jquery.nice-select.min.js') }}"></script>
-<script src="{{ asset('anime-main/js/mixitup.min.js') }}"></script>
-<script src="{{ asset('anime-main/js/jquery.slicknav.js') }}"></script>
-<script src="{{ asset('anime-main/js/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('anime-main/js/main.js') }}"></script>
+    <!-- Js Plugins -->
+    <script src="{{ asset('anime-main/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('anime-main/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('anime-main/js/player.js') }}"></script>
+    <script src="{{ asset('anime-main/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('anime-main/js/mixitup.min.js') }}"></script>
+    <script src="{{ asset('anime-main/js/jquery.slicknav.js') }}"></script>
+    <script src="{{ asset('anime-main/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('anime-main/js/main.js') }}"></script>
 
-<!-- start pagination script -->
-<script src="{{ asset('https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js') }}"></script>
+    <!-- start pagination script -->
+    <script src="{{ asset('https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js') }}"></script>
 
-<script type="text/javascript" >
-$(document).ready(function() {
-    $('#datatable').DataTable();
-    });
-</script>
-<!-- end pagination script -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#datatable').DataTable();
+        });
+    </script>
+    <!-- end pagination script -->
 
 </body>
 @include('sweetalert::alert')
+
 </html>
