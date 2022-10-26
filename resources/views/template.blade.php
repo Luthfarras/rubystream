3b006e8cba04a267a88c5bbd7097130e08e9f24c
asaddin02 <114129501+asaddin02@users.noreply.github.com>
2022-10-26 13:45:33 +0700

    eeeee

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -43,9 +43,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-2">
-                    <div class="header__logo">
                     <div class="header__logo p-1">
-                        <a href="" >
+                        <a href="">
                             <img src="{{ asset('anime-main/img/logo.png') }}" alt="">
                         </a>
                     </div>
