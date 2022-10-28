2f4622b93c0ad2686eda7504b3f6278d8b47284c
Luthfarras <luthfarras_tresanizega_27rpl@student.smktelkom-mlg.sch.id>
2022-10-28 13:34:52 +0700

    pus

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-// use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
